93c6eff6b562a60e9ad94570d909f8b2e175ac1e
Ricardo Pereira Marques <ricardo80@gmail.com>
2021-04-27 15:41:05 -0300

    DevsNotes API (estrutural): Base

--- return.php
+++ return.php
@@ -2,7 +2,7 @@
 //DEIXA TODO MUNDO ACESSAR
 header("Access-Control-Allow-Origin: *"); 
 // OS TIPOS DE REQUISIÇÕES PERMITIDAS
-header("Acess-Control-Allow-Method: GET, POST, PUT, DELETE, OPTIONS"); 
+header("Access-Control-Allow-Method: GET, POST, PUT, DELETE, OPTIONS"); 
 //RETORNO QUE VAI DAR
 header("Content-Type: application/json");
 
